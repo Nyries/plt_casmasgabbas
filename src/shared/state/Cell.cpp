ee9f2d8e4e40f804e519534237dddf894ca6d7c0
CassandreChandelier <cassandre.chandelier@ensea.fr>
2024-11-13 09:53:03 +0100

    functions Cell.cpp

--- src/shared/state/Cell.cpp
+++ src/shared/state/Cell.cpp
@@ -5,4 +5,22 @@
 namespace state {
     Cell::Cell(LocationType type, int coordX, int coordY): Location(type), coordX(coordX), coordY(coordY) {
     }
+
+    int Cell::getX() {
+        return coordX;
+    }
+
+    int Cell::getY() {
+        return coordY;
+    }
+
+    bool Cell::getOccupied() const {
+        return occupied;
+    }
+
+    void Cell::setOccupied(bool occupied) {
+        this->occupied = occupied;
+    }
+
+
 }
