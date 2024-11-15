2ab026f5afe17366b9ebafe623680a109c1f68bc
lmmas <louismmassin@gmail.com>
2024-11-15 08:23:39 +0100

    state.dia update

---
+++ src/shared/state/PlayerInfo.cpp
@@ -0,0 +1,21 @@
+//
+// Created by louismmassin on 11/8/24.
+//
+#include "PlayerInfo.h"
+
+#include <utility>
+
+namespace state {
+    PlayerInfo::PlayerInfo(State& currentGame, std::string  name, Suspect identity): currentGame(currentGame),
+        location(nullptr), name(std::move(name)), identity(identity), canWin(true) {
+    }
+
+    void PlayerInfo::setLocation(Location &newLocation) {
+        this->location = &newLocation;
+    }
+
+    void PlayerInfo::giveCard(Card &card) {
+        cards.push_back(std::move(card));
+    }
+
+}
