4b7b9bb70b9a02804624e769871b6a3078fde9d7
lmmas <louismmassin@gmail.com>
2024-11-18 22:07:43 +0100

    moved utility functions to the engine

--- src/shared/engine/UtilityFunctions.cpp
+++ src/shared/engine/UtilityFunctions.cpp
@@ -1,16 +1,20 @@
 //
-// Created by louismmassin on 10/23/24.
+// Created by louismmassin on 11/18/24.
 //
-#include "UtilityFunctions.h"
-
 #include <random>
 
-namespace state {
+#include "engine.h"
+
+namespace engine {
     int UtilityFunctions::randomInt(int range) {
         static std::random_device generator;
         static std::uniform_int_distribution<int> distribution(0,range -1);
         const int randomResult = distribution(generator);
         return randomResult;
+    }
+
+    Json::Value UtilityFunctions::jsonConversion(std::string jsonPath) {
+
     }
 
 }
