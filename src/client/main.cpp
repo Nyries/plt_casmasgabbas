ff0109ad5c94dc840c404ee4729ff375474c0301
Nyries <cyprien.gaborieau@gmail.com>
2024-12-13 14:09:34 +0100

    tests

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -19,9 +19,26 @@ int main(int argc,char* argv[])
 {
     //test();
     //client::Client client("../configurations/client.json");
-    sf::RenderWindow window(sf::VideoMode(1600, 900), "Cluedo plt");
-    window.setVerticalSyncEnabled(true);
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    sf::RenderWindow window(sf::VideoMode(desktop.height, desktop.height), "Cluedo plt", sf::Style::Close);
     render::Scene scene(window);
+    /*window.setVerticalSyncEnabled(false);
+    window.setFramerateLimit(60); // Réduit la charge sur le processeur.
+
+    while (window.isOpen())
+    {
+        // on traite tous les évènements de la fenêtre qui ont été générés depuis la dernière itération de la boucle
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // fermeture de la fenêtre lorsque l'utilisateur le souhaite
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.display();
+    }*/
+
+
     //Map map("../configurations/map.json");
     //vector<vector<Cell>> mapGrid = map.getMapGrid();
     //state::State state("../configurations/state.json");
