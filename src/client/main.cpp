ed3f45e141c77a5eb5955a80b9a5ba7f5c926274
Nyries <cyprien.gaborieau@gmail.com>
2024-12-18 09:58:33 +0100

    main

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 
 // The following lines are here to check that SFML is installed and working
-#include <engine.h>
 #include <SFML/Graphics.hpp>
 
 void testSFML() {
@@ -10,6 +9,7 @@ void testSFML() {
 }
 // end of test SFML
 
+#include <engine.h>
 #include <state.h>
 #include <client.h>
 #include <render.h>
@@ -18,34 +18,11 @@ using namespace std;
 
 int main(int argc,char* argv[])
 {
-    //test();
-    //client::Client client("../configurations/client.json");
-    state::Map map("../configurations/map.json");
-    /*sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    testSFML();
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
     sf::RenderWindow window(sf::VideoMode(desktop.height, desktop.height), "Cluedo plt", sf::Style::Close);
-    render::Scene scene(window);*/
-    /*window.setVerticalSyncEnabled(false);
-    window.setFramerateLimit(60); // Réduit la charge sur le processeur.
+    render::Scene scene(window);
 
-    while (window.isOpen())
-    {
-        // on traite tous les évènements de la fenêtre qui ont été générés depuis la dernière itération de la boucle
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // fermeture de la fenêtre lorsque l'utilisateur le souhaite
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.display();
-    }*/
-
-
-    //Map map("../configurations/map.json");
-    //vector<vector<Cell>> mapGrid = map.getMapGrid();
-    //state::State state("../configurations/state.json");
-    //cout << mapGrid[25][27].getTypeAsString() << endl;
-    //cout << 1/2 << endl;
 
 
     return 0;
