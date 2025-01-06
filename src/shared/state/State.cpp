ee77906b66e4426df4f6a6e779ca1b1b9270eeba
lmmas <louismmassin@gmail.com>
2025-01-06 17:04:27 +0100

    fixed wrong starting positions

--- src/shared/state/State.cpp
+++ src/shared/state/State.cpp
@@ -42,17 +42,17 @@ namespace state {
     Location &State::suspectToStartingCell(Suspect suspect) {
         switch (suspect) {
             case ROSE:
-                return map.getCell(15,1);
+                return map.getCell(10,25);
             case PERVENCHE:
-                return map.getCell(17,25);
+                return map.getCell(24,7);
             case LEBLANC:
-                return map.getCell(9,25);
-            case OLIVE:
                 return map.getCell(10,1);
+            case OLIVE:
+                return map.getCell(15,1);
             case MOUTARDE:
-                return map.getCell(1,8);
-            case VIOLET:
                 return map.getCell(1,18);
+            case VIOLET:
+                return map.getCell(24,20);
             default:
                 throw std::invalid_argument("Incorrect Suspect enum value");
         }
