c8a886d92d8afd75fdf2eeeee97397a54fc7f80d
Nyries <cyprien.gaborieau@gmail.com>
2025-01-15 16:02:59 +0100

    New grid

---
+++ src/client/render/RenderCell.cpp
@@ -0,0 +1,12 @@
+#include "RenderCell.h"
+
+#include <SFML/Graphics.hpp>
+
+render::RenderCell::RenderCell (float x, float y, float width, float height, int gridX, int gridY)
+: gridX(gridX), gridY(gridY){
+        setPosition(x, y);
+        setSize({width, height});
+        setFillColor(sf::Color(255, 255, 153));
+        setOutlineColor(sf::Color::Black);
+        setOutlineThickness(10);
+    }
